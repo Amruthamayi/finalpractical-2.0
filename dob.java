55f604f5777334c23b293d8e83ba95b594e19340
AMRUTHA <amruthamayi07@gmail.com>
2025-05-18 19:57:38 +0530

    Update dob.java

--- dob.java
+++ dob.java
@@ -1,6 +1,7 @@
 public class Main {
   public static void main(String[] args) {
-    String dob = "01/01/2000";
+    String dob = "01-01-2000"; // Changed date format from slashes to dashes
     System.out.println("My date of birth is " + dob);
   }
 }
+
